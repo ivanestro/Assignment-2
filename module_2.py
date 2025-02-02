44f452be5e3bb0f721a81256b85bdcf57339d3a0
Ivan Estropigan <iestropigan@rrc.ca>
2025-02-01 21:46:10 -0600

    Created a list for the assignment 2 just like in the lectures

--- module_2.py
+++ module_2.py
@@ -2,15 +2,9 @@
 
 # SIMPLE DATA TYPES
 
-name = "Ivan"
+name = "Ivan" 
 license = True
 current_year = 2025
-
-gst = 0.05
-pst = 0.07
-purchase_price = 60000.01
-total_price = (purchase_price * (pst + gst)) + purchase_price
-
 print(f"name:", name, "type:",type(name)) 
 print(f"has license:", license, "type:", type(license))
 print(f"this year:", current_year, "type:", type(current_year))
@@ -19,7 +13,32 @@ current_year +=1
 print(f"next year: ", current_year, "type:", type(current_year))
 
 # MATHEMATICAL OPERATIONS
+gst = 0.05 
+pst = 0.07
+purchase_price = 60000.01
+total_price = (purchase_price * (pst + gst)) + purchase_price
+
 gst = purchase_price * gst
 pst = purchase_price * pst
 print("Purchase Price:", purchase_price ,"Provincial Tax:",gst ,"Federal Tax:",pst ,"Total:", total_price)
-print(f"Purchase Price:${purchase_price:.2f}", f"Provincial Tax: ${gst: .2f}", f"Federal Tax: ${pst: .2f}", f"Total: ${total_price: .2f}")
+print(f"Purchase Price: ${purchase_price:.2f}", f"Provincial Tax: ${gst: .2f}", f"Federal Tax: ${pst: .2f}", f"Total: ${total_price: .2f}")
+
+# LISTS
+numbers = [1,2,3,4,5,6,7,8,9,10]
+print(numbers)
+
+numbers.insert(6,name)
+print(numbers)
+
+numbers.remove(9)
+print(numbers)
+
+new_list = ["A","B","C"]
+
+both_list = new_list + numbers
+print(both_list)
+
+provinces_and_territories = ("MB", "AB", "BC", "ON")
+
+ontario = provinces_and_territories[3]
+print(ontario)
